bc7f6ea8909561c3dba545d4c377fa7772ec3590
Git-Askhat <49748480+Git-Askhat@users.noreply.github.com>
2021-05-10 00:07:08 +0600

    Add files via upload

---
+++ keys/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://Askhat:askhat123@cluster0.cyahl.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY:
+    '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',
+  EMAIL_FROM: 'askat.shailoobekov@gmail.com',
+  BASE_URL: 'http://localhost:3000',
+};
